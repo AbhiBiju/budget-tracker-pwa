988dbdbe5d342f6e9ec7089eff13c8a06cd6b54d
AbhiBiju <abhinavbiju29@gmail.com>
2021-10-16 21:25:49 -0400

    fix service-worker file and add html links

--- server.js
+++ server.js
@@ -3,26 +3,27 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
 app.use(logger("dev"));
 
+app.use(express.static("public"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
-
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 // routes
-app.use(require("./routes/api.js"));
+const apiRoutes = require("./routes/api.js");
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
